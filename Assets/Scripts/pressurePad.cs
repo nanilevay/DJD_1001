9d6b7d45f7d54fdcd3f116d711bfd0e9ce2f24d9
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2019-05-26 22:43:30 +0100

    Add piano tiles to the scene

---
+++ Assets/Scripts/pressurePad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressurePad : MonoBehaviour
+{
+
+    public AudioSource KeySound;
+    private Rigidbody2D body;
+    public float force;
+    
+
+
+    void Start()
+    {
+        KeySound = GetComponent<AudioSource>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "MainPlayer")
+        {
+            KeySound.Play();
+            body.AddForce(transform.up * force);            
+        }
+    }
+
+
+    
+
+}
+    
+
